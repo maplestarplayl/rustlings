a71ab739674db1d90f3e9221fa2e475e9958c996
Li Feng <maplestarplay@gmail.com>
2024-12-12 10:46:05 +0800

    complete 11 hashmaps

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,29 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        if !scores.contains_key(team_1_name) {
+            scores.insert(team_1_name, TeamScores {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+        } else {
+            scores.entry(team_1_name).and_modify(|team| {
+                team.goals_scored += team_1_score;
+                team.goals_conceded += team_2_score;
+            });
+        }
+        if !scores.contains_key(team_2_name) {
+            scores.insert(team_2_name, TeamScores {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
+        } else {
+            scores.entry(team_2_name).and_modify(|team| {
+                team.goals_scored += team_2_score;
+                team.goals_conceded += team_1_score;
+            });
+        }
+    
     }
 
     scores
