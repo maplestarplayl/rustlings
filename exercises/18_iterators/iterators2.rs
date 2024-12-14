c0c8e1126e7c3a56555568aff2df05412a0d54ef
Li Feng <maplestarplay@gmail.com>
2024-12-14 20:27:28 +0800

    complete 16 17 18

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -7,7 +7,7 @@ fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => format!("{}{}", first.to_uppercase(), chars.as_str()),
     }
 }
 
@@ -15,14 +15,16 @@ fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    // ???
+    words.iter().map(|v| capitalize_first(v)).collect()// ???
 }
-
 // TODO: Apply the `capitalize_first` function again to a slice of string
 // slices. Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
-    // ???
+    words.iter().fold(String::new(), |mut acc, v| {
+        acc.push_str(&capitalize_first(v));
+        acc
+    })
 }
 
 fn main() {
--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 #[derive(Debug, PartialEq, Eq)]
 enum DivisionError {
     // Example: 42 / 0
@@ -11,21 +13,40 @@ enum DivisionError {
 // TODO: Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
-    todo!();
+    if b ==0 {
+        return Err(DivisionError::DivideByZero);
+    }
+    if a == i64::MIN && b < 0 {
+        return Err(DivisionError::IntegerOverflow);
+    }
+    if a % b != 0 {
+        return Err(DivisionError::NotDivisible);
+    }
+    Ok(a/b)
 }
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `Ok([1, 11, 1426, 3])`
-fn result_with_list() {
+fn result_with_list() -> Result<[i64; 4], DivisionError> {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let mut division_results = [0; 4];
+    
+    for (i, &num) in numbers.iter().enumerate() {
+        division_results[i] = match divide(num, 27) {
+            Ok(result) => result,
+            Err(e) => return Err(e),
+        };
+    }
+    
+    Ok(division_results)
 }
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
-fn list_of_results() {
+fn list_of_results() -> [Result<i64, DivisionError>; 4] {
     let numbers = [27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    division_results.collect::<Vec<_>>().try_into().unwrap()
 }
 
 fn main() {
